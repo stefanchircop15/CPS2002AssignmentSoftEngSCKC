201917c8568b73b85247fa15696790050a8a10a8
stefanchircop15 <37552712+stefanchircop15@users.noreply.github.com>
2018-04-15 11:25:21 +0200

    Revert "Position feature merge with develop"

---
+++ src/main/java/gamePackage/testClass.java
@@ -0,0 +1,11 @@
+package gamePackage;
+
+public class testClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Werl");
+    }
+
+    public String getMessage() {
+        return "hey";
+    }
+}
