8c868b369d1210a0d46a04f16e13d235bb29990a
stefanchircop15 <37552712+stefanchircop15@users.noreply.github.com>
2018-04-15 12:01:46 +0200

    Revert "Merging"

--- src/main/java/gamePackage/Position.java
+++ src/main/java/gamePackage/Position.java
@@ -1,31 +1,54 @@
 package gamePackage;
 
 //Position Class idea
-public class Position {
+class Position {
     private int x, y;
 
-    public int[] getPosition() {
+    Position() {
+        x = 0;
+        y = 0;
+    }
+
+    Position(int[] coords) {
+        if(coords.length != 2) {
+            x = 0;
+            y = 0;
+        }
+        else {
+            x = coords[0];
+            y = coords[1];
+        }
+    }
+
+    Position(int newX , int newY) {
+        x = newX;
+        y = newY;
+    }
+
+    //Returns coordinates as an int array
+    int[] getCoordinates() {
         return new int[] {x, y};
     }
 
-    public void setPosition(int a, int b) {
+    //Sets x, y
+    void setCoordinates(int a, int b) {
         x = a;
         y = b;
     }
 
-    public int getX() {
+    int getX() {
         return x;
     }
 
-    public int getY() {
+    int getY() {
         return y;
     }
 
-    public void setX(int a) {
+    void setX(int a) {
         x = a;
     }
 
-    public void setY(int a) {
+    void setY(int a) {
         y = a;
     }
 }
